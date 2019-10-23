52ff30c3a3b7de91680163fab872a82cae99f15b
Jason Kao <jason.kao@columbia.edu>
2019-10-22 23:30:28 -0400

    Deploy to gh-pages

--- script.fcf4fa86.js
+++ script.fcf4fa86.js
@@ -6,6 +6,6 @@ var e=[],t=function(t){t?n(t):e=document.querySelectorAll(".balance-text"),i();v
 },{}],"bpyv":[function(require,module,exports) {
 module.exports={USE_COVER_HED:!0,DOC_URL:"https://docs.google.com/document/d/12M_Qp9QrQBnXvVfIiPhkug_q38hyALieH7IKc0DS8UA/edit"};
 },{}],"mpVp":[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.hamburgerTrigger=o;var e=r(require("enter-view")),t=r(require("text-balancer")),n=require("../config.yml");function r(e){return e&&e.__esModule?e:{default:e}}var a=document.getElementById("navbar");function o(){a.classList.toggle("show-nav-links")}(0,e.default)({selector:n.USE_COVER_HED?".headline":".step-deck",offset:n.USE_COVER_HED?.92:.957,enter:function(){a.classList.remove("only-logo")},exit:function(){a.classList.remove("show-nav-links"),a.classList.add("only-logo")}}),t.default.balanceText(".headline, .deck, .image-overlay .image-caption");
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.hamburgerTrigger=o;var e=r(require("enter-view")),t=r(require("text-balancer")),n=require("../config.yml");function r(e){return e&&e.__esModule?e:{default:e}}var a=document.getElementById("navbar");function o(){a.classList.toggle("show-nav-links")}(0,e.default)({selector:n.USE_COVER_HED?".headline":".step-deck",offset:n.USE_COVER_HED?.66:.957,enter:function(){a.classList.remove("only-logo")},exit:function(){a.classList.remove("show-nav-links"),a.classList.add("only-logo")}}),t.default.balanceText(".headline, .deck, .image-overlay .g-image-caption");
 },{"enter-view":"nKD1","text-balancer":"uGUE","../config.yml":"bpyv"}]},{},["mpVp"], "script")
-//# sourceMappingURL=script.396c9a50.js.map
+//# sourceMappingURL=script.fcf4fa86.js.map
